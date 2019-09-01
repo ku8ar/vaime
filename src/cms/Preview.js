e7c4aedd9cdc4c4c9699ef08a019caf04f01d7bb
Kuba Juszczyk <juszczyk.jakub@gmail.com>
2019-09-01 08:40:59 +0200

    fix target

--- src/cms/Preview.js
+++ src/cms/Preview.js
@@ -1,11 +1,18 @@
-import React from "react"
+import React, { useMemo } from "react"
 import { StyleSheetManager } from "styled-components"
 
-const iframe = document.querySelector('#nc-root iframe')
-const iframeHeadElem = iframe && iframe.contentDocument.head
 
-export default ({ children }) => (
-  <StyleSheetManager target={iframeHeadElem}>
-    {children}
-  </StyleSheetManager>
-)
+export default ({ children }) => {
+  const iframeHead = useMemo(() => {
+    const iframe = document.querySelector('#nc-root iframe')
+    return iframe && iframe.contentDocument.head
+  }, [])
+
+  return iframeHead ? (
+    <StyleSheetManager target={iframeHead}>
+      <>
+        {children}
+      </>
+    </StyleSheetManager>
+  ) : children
+}
