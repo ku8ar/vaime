9e033cc02cd3327a7789b157775c567d20710c2d
Kuba Juszczyk <juszczyk.jakub@gmail.com>
2020-05-17 19:25:24 +0200

    sendgrid

--- lambda/send-contact-email/send-contact-email.js
+++ lambda/send-contact-email/send-contact-email.js
@@ -1,38 +1,27 @@
 // with thanks to https://github.com/Urigo/graphql-modules/blob/8cb2fd7d9938a856f83e4eee2081384533771904/website/lambda/contact.js
-const sendMail = require('sendmail')()
+const sgMail = require('@sendgrid/mail')
 
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 const CONTACT_EMAIL = 'juszczykjakub@gmail.com'
 
 exports.handler = (event, context, callback) => {
   const body = JSON.parse(event.body)
 
-  try {
-    // validateLength('body.details', body.details, 10, 1000)
-  } catch (e) {
-    return callback(null, {
-      statusCode: 403,
-      body: e.message
-    })
-  }
-
-  const descriptor = {
-    from: `"${body.email}" <no-reply@gql-modules.com>`,
+  const msg = {
     to: CONTACT_EMAIL,
-    subject: `${body.name} sent you a message from gql-modules.com`,
-    text: body.details
-  }
-
-  sendMail(descriptor, e => {
-    if (e) {
-      callback(null, {
-        statusCode: 500,
-        body: e.message
-      })
-    } else {
-      callback(null, {
-        statusCode: 200,
-        body: ''
-      })
-    }
-  })
+    from: body.email,
+    subject: 'sendgrid',
+    text: 'and easy to do anywhere, even with Node.js',
+    html: event.body,
+  };
+  sgMail.send(msg).then(
+    () =>       callback(null, {
+      statusCode: 200,
+      body: ''
+    }),
+    error => callback(null, {
+      statusCode: 500,
+      body: JSON.stringify(error)
+    })
+  )
 }
