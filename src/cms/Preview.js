58a82669a9790293f7b948ea9aebbbb2176650db
Kuba Juszczyk <juszczyk.jakub@gmail.com>
2020-05-17 12:31:28 +0200

    cms font montserrat

--- src/cms/Preview.js
+++ src/cms/Preview.js
@@ -1,13 +1,28 @@
-import React, { useMemo } from "react"
+import React, { useMemo, useEffect } from "react"
 import { StyleSheetManager } from "styled-components"
 
 export default ({ children }) => {
+  // styled component support
   const iframeHead = useMemo(() => {
     const iframe = document.getElementsByTagName('iframe')[0]
     if (!iframe) return null
     const iframeHeadElem = iframe.contentDocument.head
     return iframeHeadElem
   }, [])
+
+  // font support
+  useEffect(() => {
+    const iframe = document.getElementsByTagName('iframe')[0]
+    if (!iframe) return null
+    const iframeHeadElem = iframe.contentDocument.head
+
+    const link = document.createElement('link')
+    link.href='https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400&display=swap'
+    link.rel='stylesheet'
+
+    iframeHeadElem.appendChild(link)
+  }, [])
+
   return (
     <StyleSheetManager target={iframeHead}>
       {children}
