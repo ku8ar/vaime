13ff13c1006714ad08cd85fe3d0f35e83c151bf4
Kuba Juszczyk <juszczyk.jakub@gmail.com>
2020-06-07 00:23:39 +0200

    revert react

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,6 @@ module.exports = {
     'gatsby-plugin-netlify-cache',
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sitemap',
-    'gatsby-plugin-preact',
     {
       resolve: `gatsby-plugin-styled-components`,
       options: {
