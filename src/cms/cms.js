295e2518aa92f530a8e57e99ab3f06fe52c8b3d4
Kuba Juszczyk <juszczyk.jakub@gmail.com>
2020-07-09 22:43:32 +0200

    cloudinary test

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,6 +1,6 @@
 import CMS from 'netlify-cms-app'
 // import uploadcare from 'netlify-cms-media-library-uploadcare'
-// import cloudinary from 'netlify-cms-media-library-cloudinary'
+import cloudinary from 'netlify-cms-media-library-cloudinary'
 
 import HomePreview from './templates/HomePreview'
 import TourPreview from './templates/TourPreview'
@@ -8,7 +8,7 @@ import PagePreview from './templates/PagePreview'
 import GlobalPreview from './templates/GlobalPreview'
 
 // CMS.registerMediaLibrary(uploadcare);
-// CMS.registerMediaLibrary(cloudinary);
+CMS.registerMediaLibrary(cloudinary)
 
 CMS.registerPreviewTemplate('index', HomePreview)
 CMS.registerPreviewTemplate('global', GlobalPreview)
