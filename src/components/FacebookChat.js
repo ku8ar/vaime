ab5901c7089748dbf4221abbe399df3b838c6002
Kuba Juszczyk <juszczyk.jakub@gmail.com>
2020-07-11 18:45:50 +0200

    fb chat

---
+++ src/components/FacebookChat.js
@@ -0,0 +1,35 @@
+import React, { useEffect } from 'react'
+
+export default () => {
+  useEffect(() => {
+    window.fbAsyncInit = () => {
+      window.FB.init({
+        xfbml: true,
+        version: 'v7.0'
+      })
+    }
+    (function(d, s, id) {
+      /* eslint-disable */
+      var js, fjs = d.getElementsByTagName(s)[0]
+      if (d.getElementById(id)) return
+      js = d.createElement(s)
+      js.id = id
+      js.src = 'https://connect.facebook.net/pl_PL/sdk/xfbml.customerchat.js'
+      fjs.parentNode.insertBefore(js, fjs)
+    }(document, 'script', 'facebook-jssdk'))
+    /* eslint-enable */
+  }, [])
+
+  return (
+      <>
+        <div id="fb-root"></div>
+        <div className="fb-customerchat"
+          attribution='setup_tool'
+          page_id="329782944198629"
+          theme_color="#DE261D"
+          logged_in_greeting="Dzień dobry! W czym możemy pomóc? "
+          logged_out_greeting="Dzień dobry! W czym możemy pomóc? ">
+        </div>
+      </>
+  )
+}
