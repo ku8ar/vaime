eb0f092c8422eccf2a47b9b36d89bae7f58545d5
Kuba Juszczyk <juszczyk.jakub@gmail.com>
2020-07-17 20:39:43 +0200

    remove sentry/react

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,8 +2,8 @@
 
 exports.onClientEntry = function(_, pluginParams) {
   if (process.env.NODE_ENV === 'development') return
-  require.ensure(['@sentry/react'], function(require) {
-    const Sentry = require('@sentry/react')
+  require.ensure(['@sentry/browser'], function(require) {
+    const Sentry = require('@sentry/browser')
     Sentry.init({dsn: "https://bf0faff42d3d4f6cb6481b6e76e1c4c4@o417832.ingest.sentry.io/5319440"})
   })
 }
