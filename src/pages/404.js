76a3f71d2d6b2e67f4cb267e25f0f2b479bf51c7
Jakub Juszczyk <jakub.juszczyk@flipfit.com>
2022-02-21 23:51:00 +0400

    t

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Layout from '../components/Layout'
 import NotFoundPageStyled from '../components/NotFoundPage'
 
+
 const NotFoundPage = () => (
   <Layout title='404' description='Nie znaleziono strony' slug='/404/'>
     <NotFoundPageStyled />
